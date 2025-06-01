5ac6523f6c2ee11052d24b5ba30082443ca5c9ad
AT-Lorlando <rcd.jeremy@gmail.com>
2025-06-01 22:41:07 +0200

    :fire: No more fonts

--- install/install.sh
+++ install/install.sh
@@ -14,9 +14,6 @@ BASHRC="$HOME/.bashrc"
 echo "🔧 Installation d'Oh My Posh via le script officiel..."
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
-echo "🔤 Installation de la police Meslo..."
-eval "\$(oh-my-posh font install meslo)"
-
 echo "📁 Création du dossier de thème personnel..."
 mkdir -p "$THEME_DIR"
 
