53930f6ecd460ec933751c4a416d07c18e898dc6
AT-Lorlando <rcd.jeremy@gmail.com>
2025-11-25 15:30:20 +0100

    feat: Add addons aliases and configs

--- zsh/addons.sh
+++ zsh/addons.sh
@@ -9,7 +9,11 @@ if command -v eza >/dev/null 2>&1; then
     alias ll='eza -alhF --icons --color'
     alias la='eza -A --icons --color'
     alias l='eza -CF --icons --color'
-    alias tree='eza --tree --icons --color'
+    alias tree='eza --tree --icons --color -L 3'
 fi
 
 export EDITOR="nano"
+
+compdef _files eza
+
+export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
